435b1acc52b33351163efc74a593262253ed02b5
yasmws <57499139+yasmws@users.noreply.github.com>
2022-06-30 22:51:01 -0300

    Lista1

---
+++ Lista1/questao1b.c
@@ -0,0 +1,195 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "stdbool.h"
+#include "math.h"
+
+typedef struct link
+{
+    char element;
+    struct link *next;
+} Link;
+
+typedef struct list
+{
+    Link *head;
+    Link *tail;
+    Link *curr;
+    int cnt;
+} List;
+
+Link *create_link2(char it, Link *nextvalue)
+{
+    Link *n = (Link *)malloc(1 * sizeof(Link));
+    n->element = it;
+    n->next = nextvalue;
+    return n;
+}
+
+Link *create_link(Link *nextvalue)
+{
+    Link *n = (Link *)malloc(1 * sizeof(Link));
+    if (n == NULL)
+    {
+        printf("Tudo errado no link");
+    }
+
+    n->next = nextvalue;
+    return n;
+}
+
+List *create_list()
+{
+
+    List *l = (List *)malloc(1 * sizeof(List));
+    if (l == NULL)
+    {
+        printf("\nTudo errado na lista");
+    }
+
+    l->head = create_link(NULL); // header node
+    l->tail = l->head;
+    l->curr = l->tail;
+    l->cnt = 0;
+    return l;
+}
+
+void insert(List *l, char it)
+{
+    l->curr->next = create_link2(it, l->curr->next);
+    if (l->tail == l->curr)
+    {
+        l->tail = l->curr->next;
+    }
+    l->cnt++;
+}
+
+void moveToStart(List *l)
+{
+    l->curr = l->head;
+}
+
+void next(List *l)
+{
+    if (l->curr->next != l->tail)
+    {
+        l->curr = l->curr->next;
+    }
+}
+
+void bei(List *l, char *entrada, int i, int j)//i=[//j=]
+{
+
+    int k = 0, p = 0, flag=0;
+
+    for (k = i+1; k < j; k++)
+    {
+        if (entrada[k] == '[')
+        {
+            entrada[i]=' '; entrada[j]=' ';
+            k=j; flag=1;
+        }
+    }
+
+    for (k = i+1; k < j; k++)
+    {
+       if(entrada[k]!=' ' && flag==0){
+            insert(l, entrada[k]);
+            entrada[k]=' '; entrada[i]=' '; entrada[j]=' ';
+            }
+    }
+
+}
+
+void beiju(List *l, char *entrada, int i,int tam)
+{ // 5
+
+    int j = 0, flag = 1;
+
+    for (j = i + 1; j < tam; j++)//encontrando o mais perto
+    {
+        if (entrada[j] == ']')
+        {
+            bei(l, entrada, i, j);
+        }
+    }
+}
+
+List *beijuText(List *l, char *entrada)
+{
+
+    int i = 0, flag = -1; int tam = strlen(entrada);
+
+    while (i < tam)
+    {
+        if (entrada[i] == '[')
+        {                       
+            if(entrada[i+1]==']') {entrada[i]=' '; entrada[i+1]=' ';}
+            else beiju(l, entrada, i,tam); // 5
+        }
+        i++;
+    }
+
+    i = 0;
+    while (i < tam) 
+    {
+        if(entrada[i]!=' '){
+        insert(l, entrada[i]);
+        }
+        i++;
+        
+    }
+
+    return l;
+}
+
+void printText(List *l)
+{ // ok
+
+    moveToStart(l);
+    char saida[100001];
+    int i; 
+
+    for (i = 0; i < l->cnt; i++)
+    {
+        saida[i] = l->curr->next->element;
+        next(l);
+    }
+
+    while (i > 0)
+    {
+        i--;
+        printf("%c", saida[i]);
+    }
+
+    printf("\n");
+}
+
+void painelDeControle()
+{
+
+    int flag = 1;
+    char entrada[100001];
+
+    do
+    {
+        List *l = create_list();
+        if (scanf("%s", entrada) != EOF)
+        {
+            l = beijuText(l, entrada);
+            printText(l);
+            free(l);
+        }
+        else
+            flag = -1;
+
+    } while (flag > 0);
+}
+
+int main()
+{
+
+    painelDeControle();
+
+    return 0;
+}
